4f87603a1f85c4d9922547d66f84e0f367e76a07
Davi Guerreiro <daviguerreiro9a@gmail.com>
2025-11-24 22:17:20 -0300

    algumas macros úteis

---
+++ teorico/template.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+//tipos
+using ll = long long;
+using ld  = long double;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+//loops
+#define FOR(i,n) for(int i=0; i<(int)(n); i++)
+#define FOR1(i,n) for(int i=1; i<=(int)(n); i++)
+#define FORA(x,a) for(auto &x : a)
+//Sorts
+#define all(x) begin(x), end(x)
+#define rall(x) rbegin(x), rend(x)
+//debug
+#define dbg(x) cerr << #x << " = " << x << "\n"
+
+int main(){
+    ios::sync_with_stdio(false);cin.tie(0);
+    int n = 3;
+    dbg(n);
+    return 0;
+}
